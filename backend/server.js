51c5224732096653f5dee57b5fea99ff53bdf55d
abhijeetsatpute <abhijeetsatpute98@gmail.com>
2023-04-08 19:57:55 +0530

    JWT auth registerUser & authUser login, hash pswd

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
-// const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes");
 // const chatRoutes = require("./routes/chatRoutes");
 // const messageRoutes = require("./routes/messageRoutes");
 // const { notFound, errorHandler } = require("./middleware/errorMiddleware");
@@ -12,22 +12,13 @@ dotenv.config();
 connectDB();
 const app = express();
 
-// app.use(express.json()); // to accept json data
+app.use(express.json()); // to accept json data
 
 app.get("/", (req, res) => {
   res.send("API Running!");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
-
-// app.use("/api/user", userRoutes);
+app.use("/api/user", userRoutes);
 // app.use("/api/chat", chatRoutes);
 // app.use("/api/message", messageRoutes);
 
